0920d957cf9c14b0bae1ae504e2beb72b29fc7d5
hkkk <youkinn#1111@gmail.com>
2023-11-19 00:45:49 +0800

    (optimize)[yadm]: Add soft link command of .zshenv script
    Signed-off-by: hkkk <youkinn#1111@gmail.com>

--- .config/yadm/bootstrap.d/02_zsh.sh
+++ .config/yadm/bootstrap.d/02_zsh.sh
@@ -5,6 +5,7 @@ source ./libs.sh
 pacman_install zsh
 if [ ! "$SHELL" = "/bin/zsh" ]; then
     sudo chsh -s /bin/zsh "$USER"
-    find $HOME -name ".bash*" -delete
+    find "$HOME" -name ".bash*" -delete
+    ln -sf "$XDG_CONFIG_HOME/zsh/.zshenv" "$HOME/.zshenv"
     exec zsh
 fi
