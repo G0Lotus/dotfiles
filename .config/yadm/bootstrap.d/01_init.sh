79f2951777f21cc800a788c276353877ef6f8379
hkkk <youkinn#1111@gmail.com>
2023-11-19 16:06:30 +0800

    (chore)[yadm]: support to change yadm remote url to ssh style
    Signed-off-by: hkkk <youkinn#1111@gmail.com>

--- .config/yadm/bootstrap.d/01_init.sh
+++ .config/yadm/bootstrap.d/01_init.sh
@@ -8,3 +8,6 @@ sudo pacman-key --populate
 pacman_install archlinux-keyring reflector
 sudo reflector --latest 20 --fastest 20 --threads 20 --sort rate --protocol https -c China --save /etc/pacman.d/mirrorlist
 sudo pacman -Syyu --noconfirm
+yadm decrypt
+yadm remote set-url origin git@github.com:G0Lotus/dotfiles.git
+
