4c6e720b56b24df2f518b5b07de2dd90346fa1ec
hkkk <youkinn#1111@gmail.com>
2023-11-28 09:03:49 +0800

    (fix)[yadm]: .zshenv link error path
    Signed-off-by: hkkk <youkinn#1111@gmail.com>

--- .config/yadm/bootstrap.d/02_zsh.sh
+++ .config/yadm/bootstrap.d/02_zsh.sh
@@ -6,6 +6,6 @@ pacman_install zsh
 if [ ! "$SHELL" = "/bin/zsh" ]; then
     sudo chsh -s /bin/zsh "$USER"
     find "$HOME" -name ".bash*" -delete
-    ln -sf "$XDG_CONFIG_HOME/zsh/.zshenv" "$HOME/.zshenv"
+    ln -sf "$HOME/.config/zsh/.zshenv" "$HOME/.zshenv"
     exec zsh
 fi
