5160dc4493a762b67a5b6c748b2039f92265a9e0
hkkk <youkinn#1111@gmail.com>
2023-11-30 22:25:28 +0800

    (optimize)[zsh]: update the zinit clone address
    Signed-off-by: hkkk <youkinn#1111@gmail.com>

--- .config/zsh/config/zinit.zsh
+++ .config/zsh/config/zinit.zsh
@@ -2,7 +2,7 @@
 
 ZINIT_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}/zinit/zinit.git"
 [ ! -d $ZINIT_HOME ] && mkdir -p "$(dirname $ZINIT_HOME)"
-[ ! -d $ZINIT_HOME/.git ] && git clone https://github.hscsec.cn/zdharma-continuum/zinit.git "$ZINIT_HOME"
+[ ! -d $ZINIT_HOME/.git ] && git clone https://github.com/zdharma-continuum/zinit.git "$ZINIT_HOME"
 
 # typeset -A ZINIT=(
 #     BIN_DIR  $HOME/.config/zsh/zinit/bin
